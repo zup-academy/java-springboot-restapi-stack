9c58af3e85860692f47fef6b87af15272e334be4
Rafael Ponte <rafael.ponte@zup.com.br>
2022-08-15 15:38:09 -0300

    fix pom.xml and adds basica test class in the sourcefolder test/java;

---
+++ default-template/templates/src/test/java/{{computed_inputs.project_folder}}/ApplicationTests.java
@@ -0,0 +1,13 @@
+package {{computed_inputs.project_base_package}};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
